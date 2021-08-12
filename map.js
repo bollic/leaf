13db2f1204ee59918804a4f4f0297cdd79e65080
bollic <48652894+bollic@users.noreply.github.com>
2021-08-12 13:38:43 +0200

    Delete map.js

--- map.js
+++
@@ -1,9 +0,0 @@
-var map = L.map('map').setView([43.5932,1.3951], 17);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-var marker = L.marker([43.0697,14.4432]).addTo(map)
-		.bindPopup('The Technology Sandbox<br> Where Yoh is sitting this very moment')
-		.openPopup();	
